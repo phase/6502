81a38fb883ab1dc69c99b87c30c268518ce1d488
phase <ph4sesaber@gmail.com>
2015-04-24 02:20:20 -0700

    Add comments to diveOnIn.asm

--- asm/examples/diveOnIn.asm
+++ asm/examples/diveOnIn.asm
@@ -1,6 +1,6 @@
-lda #$01
-sta $0200
-lda #$05
-sta $0201
-lda #$08
-sta $0202
+lda #$01  ;Load 01 to A
+sta $0200 ;Store A in 0200
+lda #$05  ;Load 05 to A
+sta $0201 ;Store A in 0201
+lda #$08  ;Load 08 in A
+sta $0202 ;Store A in 0202
