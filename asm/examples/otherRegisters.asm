200154d58215edf6a7c6c8fa36a3beaad19feaef
phase <ph4sesaber@gmail.com>
2015-04-24 02:21:23 -0700

    Add comments to otherRegisters.asm

--- asm/examples/otherRegisters.asm
+++ asm/examples/otherRegisters.asm
@@ -1,6 +1,6 @@
-ldx #$0d
-stx $0200
-txa
-inx
-stx $0201
-sta $0202
+ldx #$0d  ;Load 0d in X
+stx $0200 ;Store X in 0200
+txa       ;Transfer X to A
+inx       ;Increment X
+stx $0201 ;Store X in 0201
+sta $0202 ;Store A in 0202
